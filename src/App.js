b9de7727a085e3927fbee907b522fe58478b849a
heather van der Dys <heather.vanderdys@gmail.com>
2019-02-24 16:18:00 +0200

    Global styles and featured text component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import FeaturedText from './components/FeaturedText';
 
 export default function App() {
   return (
     <div className="app">
       <Header />
+      <FeaturedText />
       <button>Button</button>
     </div>
   );
---
+++ src/components/FeaturedText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function FeaturedText() {
+  return (
+    <div>
+      <h2>Heading</h2>
+      <h3>sub heading</h3>
+      <p className="content">content</p>
+    </div>
+  );
+}
