a074fd34cdc15cdc5fd65d83f45d4b02c740beac
heather van der Dys <heather.vanderdys@gmail.com>
2019-02-24 19:20:31 +0200

    Tabor page

--- src/pages/Tabor.js
+++ src/pages/Tabor.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import Logo from '../components/Logo';
+import Countdown from '../components/Countdown';
+import FeaturedText from '../components/FeaturedText';
+import Bios from '../components/Bios';
 
 export default function Tabor() {
   return (
-    <div>
-      <h1>Tabor</h1>
-      <p className="content">content</p>
+    <div className="app">
+      <div className="tabor-header section">
+        <Logo />
+        <Countdown fromDate="3/31/2019" />
+        <FeaturedText
+          title="What is the Csakis Tabor?"
+          subtitle="In Essence: Family Camp"
+          content="Lets escape."
+        />
+      </div>
+      <div className="tabor-bio section">
+        <Bios
+          title="Meet the Speakers"
+          bios={[
+            {
+              content:
+                'Pellentesque blandit metus vitae metus convallis mattis. Integer ac risus eu dolor.',
+              image: '//placehold.it/100',
+              alt: 'name of speaker'
+            },
+            {
+              content:
+                'Convallis mattis. Integer ac risus eu dolor. Yep okay that is the one.',
+              image: '//placehold.it/100',
+              alt: 'name of speaker 2'
+            }
+          ]}
+        />
+        <FeaturedText />
+      </div>
     </div>
   );
 }
