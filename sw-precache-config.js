d13c58f74ce3396d4ef095f0004b8be001bffaab
Richard van der Dys <richard.vanderdys@gmail.com>
2019-02-24 11:57:19 +0200

    Commit from create-static-app

---
+++ sw-precache-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  staticFileGlobs: [
+    'build/static/css/**.css',
+    'build/static/js/**.js',
+    'build/**/**.html',
+    'build/favicon.png'
+  ],
+  stripPrefix: 'build/',
+  runtimeCaching: []
+};
