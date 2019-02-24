1ad96ee08cf77ab15bb01df326bb970d0c555b03
Richard van der Dys <richard.vanderdys@gmail.com>
2019-02-24 21:28:25 +0200

    Added logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import { ReactComponent as Img } from '../images/logo-coloredCsakis2019.svg';
 
 export default function Logo({ title }) {
   return (
-    <header>
+    <header className="logo">
       <Img />
       {title ? <h1>{title}</h1> : null}
     </header>
