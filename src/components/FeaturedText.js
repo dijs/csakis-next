593d450bf9c04034036c88a38176c3b3dffb03fa
heather van der Dys <heather.vanderdys@gmail.com>
2019-02-24 17:41:56 +0200

    a lot sorry

--- src/components/FeaturedText.js
+++ src/components/FeaturedText.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function FeaturedText() {
+export default function FeaturedText({ title, subtitle, content = [] }) {
   return (
     <div>
-      <h2>Heading</h2>
-      <h3>sub heading</h3>
-      <p className="content">content</p>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p className="content">{content}</p>
     </div>
   );
 }
--- src/pages/Delutan.js
+++ src/pages/Delutan.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Header';
+import Logo from '../components/Logo';
 import Countdown from '../components/Countdown';
 import FeaturedText from '../components/FeaturedText';
 import Bios from '../components/Bios';
@@ -8,23 +8,29 @@ export default function Delutan() {
   return (
     <div>
       <div className="app">
-        <div className="section">
-          <Header />
-          <FeaturedText />
+        <div className="delutan-header section">
+          <Logo />
           <Countdown fromDate="3/31/2019" />
+          <FeaturedText
+            title="What is Csakis Delutan?"
+            subtitle="Praise - Sermon - Fellowship"
+            content="We rarely have an opportunity to build our marriages. Often we get cought up in the daily toil of tasks and we do not have quality time with our spouse and/or our children. This afternoon event is dedicated to helping fostor this quality time and growth. It will be organised quarterly in Vigadó. During the lectures, we will provide separate supervision for children, where we teach them about the love of God through biblical stories."
+          />
+        </div>
+        <div className="delutan-bio section">
           <Bios
             title="Meet the Speakers"
             bios={[
               {
                 content:
                   'Pellentesque blandit metus vitae metus convallis mattis. Integer ac risus eu dolor.',
-                image: '//placehold.it/200',
+                image: '//placehold.it/100',
                 alt: 'name of speaker'
               },
               {
                 content:
                   'Convallis mattis. Integer ac risus eu dolor. Yep okay that is the one.',
-                image: '//placehold.it/300',
+                image: '//placehold.it/100',
                 alt: 'name of speaker 2'
               }
             ]}
