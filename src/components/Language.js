ca7c53a9264bb81f06bd994b34aa33bfb75b7653
Richard van der Dys <richard.vanderdys@gmail.com>
2019-02-24 21:05:48 +0200

    Fixed lang bug

--- src/components/Language.js
+++ src/components/Language.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Link, withRouter, matchPath } from 'react-router-dom';
 
-function Language({ history, close }) {
-  const match = matchPath(history.location.pathname, {
+function getParams(pathname) {
+  const match = matchPath(pathname, {
     path: '/:lang?/:page?'
   });
-  const nextLang = match.params.lang === 'en' ? 'hu' : 'en';
-  const nextTo = `/${nextLang}/${match.params.page || ''}`;
-  const nextLangName = match.params.lang === 'en' ? 'Magyar' : 'English';
+  const {
+    params: { lang, page }
+  } = match;
+  if (!page && lang) {
+    return {
+      lang: 'hu',
+      page: lang
+    };
+  }
+  return {
+    lang,
+    page
+  };
+}
+
+function Language({ history, close }) {
+  const { lang, page } = getParams(history.location.pathname);
+  const nextLang = lang === 'en' ? 'hu' : 'en';
+  const nextTo = `/${nextLang}/${page || ''}`;
+  const nextLangName = lang === 'en' ? 'Magyar' : 'English';
   return (
     <Link to={nextTo} onClick={close}>
       {nextLangName}
