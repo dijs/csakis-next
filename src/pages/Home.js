5b0bcdb7b6827a7c01e2449a0f1bc7178481597c
heather van der Dys <heather.vanderdys@gmail.com>
2019-02-24 19:51:55 +0200

    home page initial build and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Logo from '../components/Logo';
+import FeaturedText from '../components/FeaturedText';
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <p className="content">content</p>
+      <div className="home-header section">
+        <Logo />
+      </div>
+      <div className="home-text section">
+        <FeaturedText
+          title="What is Csakis?"
+          subtitle="subtitle"
+          content="content"
+        />
+      </div>
+      <div className="home-photos section">
+        <h2> Photo Gallery </h2>
+      </div>
+      <div className="home-events section">
+        <h2> Upcoming Events </h2>
+      </div>
     </div>
   );
 }
