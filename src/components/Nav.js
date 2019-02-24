b1c413efdcb79d35cbc53c32c03c1a0c0c1cfc87
Richard van der Dys <richard.vanderdys@gmail.com>
2019-02-24 19:57:34 +0200

    Fixed router

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,13 +9,13 @@ function Nav({ store }) {
   const close = () => (store.open = false);
   return (
     <div className={classnames('nav', { open: store.open })}>
-      <Link to="/" onClick={close}>
+      <Link to="./" onClick={close}>
         Home
       </Link>
-      <Link to="/delutan" onClick={close}>
+      <Link to="./delutan" onClick={close}>
         Delutan
       </Link>
-      <Link to="/tabor" onClick={close}>
+      <Link to="./tabor" onClick={close}>
         Tabor
       </Link>
     </div>
