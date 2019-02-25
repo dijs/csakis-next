069cc2456e61c33c6cb9fadec563f4b0991c2b20
heather van der Dys <heather.vanderdys@gmail.com>
2019-02-25 13:10:14 +0200

    ammended hiararchy in featured text

--- src/components/FeaturedText.js
+++ src/components/FeaturedText.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function FeaturedText({ title, subtitle, content = [] }) {
   return (
-    <div>
+    <div className="featured">
       <h2>{title}</h2>
       <h3>{subtitle}</h3>
       <p className="content">{content}</p>
