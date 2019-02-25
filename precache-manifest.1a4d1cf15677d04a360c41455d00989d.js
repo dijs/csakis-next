bc81e0a914a38af9a502001d6279ad7c3fe399bd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-25 13:19:32 +0000

    Deploy dijs/csakis-next to github.com/dijs/csakis-next.git:gh-pages

---
+++ precache-manifest.1a4d1cf15677d04a360c41455d00989d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8026bc2fde7037c1c1f81cf3dff858f",
+    "url": "/csakis-next/static/media/calendar.f8026bc2.svg"
+  },
+  {
+    "revision": "acd069ca58ff34a4fa2c",
+    "url": "/csakis-next/static/css/main.0abee688.chunk.css"
+  },
+  {
+    "revision": "208188a91c8d4c32b23e",
+    "url": "/csakis-next/static/js/runtime~main.208188a9.js"
+  },
+  {
+    "revision": "ecc555950e524f74db98be1ac846a58b",
+    "url": "/csakis-next/static/media/delete-ex.ecc55595.svg"
+  },
+  {
+    "revision": "54254a44497d269e3b8e",
+    "url": "/csakis-next/static/js/2.54254a44.chunk.js"
+  },
+  {
+    "revision": "c00223632a33a6bcbdc42f2b6777744a",
+    "url": "/csakis-next/static/media/logo-coloredCsakis2019withCircle.c0022363.svg"
+  },
+  {
+    "revision": "28e93a4d67780485ba2d2a5dbdeb12ac",
+    "url": "/csakis-next/static/media/logo-whiteCsakis2019.28e93a4d.svg"
+  },
+  {
+    "revision": "acd069ca58ff34a4fa2c",
+    "url": "/csakis-next/static/js/main.acd069ca.chunk.js"
+  },
+  {
+    "revision": "4afbf97a29c1e0b8dcdff302ab4a191c",
+    "url": "/csakis-next/static/media/location.4afbf97a.svg"
+  },
+  {
+    "revision": "7d01ed2408017e4dd87d23b9acad3c7a",
+    "url": "/csakis-next/static/media/facebook.7d01ed24.svg"
+  },
+  {
+    "revision": "19ac4a5f591fe05774e4ec2ae84fbafa",
+    "url": "/csakis-next/static/media/logo-coloredCsakis2019.19ac4a5f.svg"
+  },
+  {
+    "revision": "4c36debedcb8459fd418c5ac1c78bf25",
+    "url": "/csakis-next/static/media/three-dot-menu.4c36debe.svg"
+  },
+  {
+    "revision": "54254a44497d269e3b8e",
+    "url": "/csakis-next/static/css/2.0401954f.chunk.css"
+  },
+  {
+    "revision": "67f46c573577ac18d52032b55f7c92cb",
+    "url": "/csakis-next/index.html"
+  }
+];
