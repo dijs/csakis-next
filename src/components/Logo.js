8d999c08834b1507b36dc713fedc73ff4e4e5a5e
Richard van der Dys <richard.vanderdys@gmail.com>
2019-03-01 12:31:35 +0200

    Added media queries and logo link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { ReactComponent as Img } from '../images/logo-coloredCsakis2019withCircle.svg';
+import { Link } from 'react-router-dom';
 
 export default function Logo({ title }) {
   return (
     <header className="logo">
-      <Img />
+      <Link to="/">
+        <Img />
+      </Link>
       {title ? <h1>{title}</h1> : null}
     </header>
   );
