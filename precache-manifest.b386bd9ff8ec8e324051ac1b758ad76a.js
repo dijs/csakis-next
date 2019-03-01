5287f63fbc17db8d133aac9800bcfaab37082ac8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-01 11:06:54 +0000

    Deploy dijs/csakis-next to github.com/dijs/csakis-next.git:gh-pages

---
+++ precache-manifest.b386bd9ff8ec8e324051ac1b758ad76a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9d866e4f8d1528f0a106bd76a35722a",
+    "url": "/static/media/information.b9d866e4.svg"
+  },
+  {
+    "revision": "ded9553229566c50849b",
+    "url": "/static/css/main.c4c06989.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ecc555950e524f74db98be1ac846a58b",
+    "url": "/static/media/delete-ex.ecc55595.svg"
+  },
+  {
+    "revision": "1087bcf0e08d32679b84",
+    "url": "/static/js/2.1087bcf0.chunk.js"
+  },
+  {
+    "revision": "c00223632a33a6bcbdc42f2b6777744a",
+    "url": "/static/media/logo-coloredCsakis2019withCircle.c0022363.svg"
+  },
+  {
+    "revision": "28e93a4d67780485ba2d2a5dbdeb12ac",
+    "url": "/static/media/logo-whiteCsakis2019.28e93a4d.svg"
+  },
+  {
+    "revision": "634437141ddee7eea11acaaee4b973cc",
+    "url": "/static/media/photo-gallery.63443714.svg"
+  },
+  {
+    "revision": "ded9553229566c50849b",
+    "url": "/static/js/main.ded95532.chunk.js"
+  },
+  {
+    "revision": "7d01ed2408017e4dd87d23b9acad3c7a",
+    "url": "/static/media/facebook.7d01ed24.svg"
+  },
+  {
+    "revision": "18a6ac9e2b70f9a6f3359796c6a8f578",
+    "url": "/static/media/mail-icon.18a6ac9e.svg"
+  },
+  {
+    "revision": "19ac4a5f591fe05774e4ec2ae84fbafa",
+    "url": "/static/media/logo-coloredCsakis2019.19ac4a5f.svg"
+  },
+  {
+    "revision": "4afbf97a29c1e0b8dcdff302ab4a191c",
+    "url": "/static/media/location.4afbf97a.svg"
+  },
+  {
+    "revision": "4c36debedcb8459fd418c5ac1c78bf25",
+    "url": "/static/media/three-dot-menu.4c36debe.svg"
+  },
+  {
+    "revision": "1087bcf0e08d32679b84",
+    "url": "/static/css/2.0401954f.chunk.css"
+  },
+  {
+    "revision": "1f3262bc7358df20e0b39df7d06199c1",
+    "url": "/index.html"
+  }
+];
