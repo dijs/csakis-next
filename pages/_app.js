76f7fe71581a6872405c5108b7ef6f3c233aa279
Richard van der Dys <richard.vanderdys@gmail.com>
2021-06-07 15:21:29 +0300

    Fixed metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
-import '../styles/index.scss';
 import Nav from '../components/Nav';
 import NavToggle from '../components/NavToggle';
 import { StoreProvider } from '../components/Store';
+import Head from 'next/head';
+import '../styles/index.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <title>Csakis</title>
+      </Head>
       <StoreProvider>
         <Nav />
         <NavToggle />
