8ee1d5aa883f7dfb70bc245ae7d6b864529037cf
Richard van der Dys <richard.vanderdys@gmail.com>
2021-06-29 19:39:26 +0300

    Fixed default HU lang

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   i18n: {
-    locales: ['en', 'hu'],
-    defaultLocale: 'hu'
+    locales: ['hu', 'en'],
+    defaultLocale: 'hu',
+    localeDetection: false
   },
   webpack(config) {
     config.module.rules.push({
