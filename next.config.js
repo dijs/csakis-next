5064efd64fa4774accfbb85713944b07a722aa2a
richardvanderdys <richard.vanderdys@smarterdx.com>
2025-03-26 08:41:59 -0500

    Upgrading to Next 15

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/
-      },
       use: ['@svgr/webpack']
     });
     return config;
