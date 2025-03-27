253177fa881478e51276095419c07d8beea202e5
Heather van der Dys <heather.vanderdys@gmail.com>
2025-03-27 15:11:16 -0500

    45 min: added survey and updated dates plus added registration date

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,14 @@ export default function Home() {
       </div>
       <div className="home-text section">
         <FeaturedText {..._.homeFeatured} />
+        <FeaturedText {..._.homeSurvey} />
+        <Link className="survey" href="https://docs.google.com/forms/d/1q8MyRm774KDw_q8X98G7hS6y6GtyLHBmgJ-MFSSe8Sk/viewform?edit_requested=true"><button>Survey</button></Link>
       </div>
       <div className="home-events section">
         <h2> {_.homeEventTitle} </h2>
         <EventCard {..._.homeEventOne} />
         <EventCard {..._.homeEventTwo} />
+        <EventCard {..._.homeEventThree} />
       </div>
       <Album />
       <Footer />
